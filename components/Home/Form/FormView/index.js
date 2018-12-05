4dd51f962fef39b1bf915838a2a1c20fd92ca257
John Tucker <jtucker@appirio.com>
2018-12-04 20:27:57 -0500

    Example

--- components/Home/Form/FormView/index.js
+++ components/Home/Form/FormView/index.js
@@ -7,18 +7,37 @@ import styles from './styles';
 class FormView extends PureComponent {
   render() {
     const {
+      onABlur,
       onAChangeText,
+      onBBlur,
       onBChangeText,
+      onBInputRef,
+      onBLabelRef,
+      onCBlur,
+      onCInputRef,
+      onCLabelRef,
       onCChangeText,
       onDPress,
+      onDRef,
+      onEBlur,
+      onEChangeText,
+      onEInputRef,
+      onELabelRef,
+      onScrollerRef,
       valueA,
       valueB,
       valueC,
       valueD,
+      valueE,
     } = this.props;
     return (
-      <KeyboardAwareScrollView style={styles.form}>
-        <Text style={styles.formText}>
+      <KeyboardAwareScrollView
+        ref={onScrollerRef}
+        style={styles.form}
+      >
+        <Text
+          style={styles.formText}
+        >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Maecenas quis tincidunt ex. Nulla vulputate nulla quam,
           sed rutrum magna dignissim ut. Praesent eu ante at risus
@@ -26,6 +45,8 @@ class FormView extends PureComponent {
         </Text>
         <Text style={styles.formLabel}>A</Text>
         <TextInput
+          autoFocus
+          onBlur={onABlur}
           onChangeText={onAChangeText}
           style={styles.formTextInput}
           value={valueA}
@@ -36,9 +57,16 @@ class FormView extends PureComponent {
           sed rutrum magna dignissim ut. Praesent eu ante at risus
           efficitur pellentesque.
         </Text>
-        <Text style={styles.formLabel}>B</Text>
+        <Text
+          ref={onBLabelRef}
+          style={styles.formLabel}
+        >
+          B
+        </Text>
         <TextInput
+          onBlur={onBBlur}
           onChangeText={onBChangeText}
+          ref={onBInputRef}
           style={styles.formTextInput}
           value={valueB}
         />
@@ -48,9 +76,16 @@ class FormView extends PureComponent {
           sed rutrum magna dignissim ut. Praesent eu ante at risus
           efficitur pellentesque.
         </Text>
-        <Text style={styles.formLabel}>C</Text>
+        <Text
+          ref={onCLabelRef}
+          style={styles.formLabel}
+        >
+          C
+        </Text>
         <TextInput
+          onBlur={onCBlur}
           onChangeText={onCChangeText}
+          ref={onCInputRef}
           style={styles.formTextInput}
           value={valueC}
         />
@@ -60,24 +95,62 @@ class FormView extends PureComponent {
           sed rutrum magna dignissim ut. Praesent eu ante at risus
           efficitur pellentesque.
         </Text>
-        <Text style={styles.formLabel}>D</Text>
+        <Text
+          ref={onDRef}
+          style={styles.formLabel}
+        >
+          D
+        </Text>
         <TouchableOpacity onPress={onDPress}>
           <Text style={styles.formAutocomplete}>{valueD}</Text>
         </TouchableOpacity>
+        <Text style={styles.formText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Maecenas quis tincidunt ex. Nulla vulputate nulla quam,
+          sed rutrum magna dignissim ut. Praesent eu ante at risus
+          efficitur pellentesque.
+        </Text>
+        <Text
+          ref={onELabelRef}
+          style={styles.formLabel}
+        >
+          E
+        </Text>
+        <TextInput
+          onBlur={onEBlur}
+          onChangeText={onEChangeText}
+          ref={onEInputRef}
+          style={styles.formTextInput}
+          value={valueE}
+        />
       </KeyboardAwareScrollView>
     );
   }
 }
 
 FormView.propTypes = {
+  onABlur: PropTypes.func.isRequired,
+  onBBlur: PropTypes.func.isRequired,
   onAChangeText: PropTypes.func.isRequired,
   onBChangeText: PropTypes.func.isRequired,
+  onBInputRef: PropTypes.func.isRequired,
+  onBLabelRef: PropTypes.func.isRequired,
+  onCBlur: PropTypes.func.isRequired,
+  onCInputRef: PropTypes.func.isRequired,
+  onCLabelRef: PropTypes.func.isRequired,
   onCChangeText: PropTypes.func.isRequired,
   onDPress: PropTypes.func.isRequired,
+  onDRef: PropTypes.func.isRequired,
+  onEBlur: PropTypes.func.isRequired,
+  onEChangeText: PropTypes.func.isRequired,
+  onEInputRef: PropTypes.func.isRequired,
+  onELabelRef: PropTypes.func.isRequired,
+  onScrollerRef: PropTypes.func.isRequired,
   valueA: PropTypes.string.isRequired,
   valueB: PropTypes.string.isRequired,
   valueC: PropTypes.string.isRequired,
   valueD: PropTypes.string.isRequired,
+  valueE: PropTypes.string.isRequired,
 };
 
 export default FormView;
--- components/Home/Form/index.js
+++ components/Home/Form/index.js
@@ -5,13 +5,41 @@ import { NavigationEvents } from 'react-navigation';
 import FormView from './FormView';
 import fetchStates from '../../../apis/states';
 
+const DELAY = 500;
+const delay = () => new Promise(resolve => setTimeout(() => resolve(), DELAY));
+
 class Form extends PureComponent {
+  refBLabel = null;
+
+  refBInput = null;
+
+  refCLabel = null;
+
+  refCInput = null;
+
+  refD = null;
+
+  refScroller = null;
+
+  refELabel = null;
+
+  refEInput = null;
+
   state = {
     valueA: '',
     valueB: '',
     valueC: '',
     valueD: '',
+    valueE: '',
   };
+
+  measureRefBY = () => new Promise(resolve => this.refBLabel.measure((_, y) => resolve(y)));
+
+  measureRefCY = () => new Promise(resolve => this.refCLabel.measure((_, y) => resolve(y)));
+
+  measureRefDY = () => new Promise(resolve => this.refD.measure((_, y) => resolve(y)));
+
+  measureRefEY = () => new Promise(resolve => this.refELabel.measure((_, y) => resolve(y)));
 
   handleAChangeText = text => this.setState({ valueA: text });
 
@@ -29,11 +57,76 @@ class Form extends PureComponent {
     });
   }
 
-  handleDidFocus = ({ state: { params: { value } = {} } }) => {
+  handleEChangeText = text => this.setState({ valueE: text });
+
+  handleABlur = async () => {
+    await delay();
+    const refBY = await this.measureRefBY();
+    this.refScroller.scrollToPosition(0, refBY);
+    await delay();
+    this.refBInput.focus();
+  };
+
+  handleBBlur = async () => {
+    await delay();
+    const refCY = await this.measureRefCY();
+    this.refScroller.scrollToPosition(0, refCY);
+    await delay();
+    this.refCInput.focus();
+  };
+
+  handleCBlur = async () => {
+    await delay();
+    const refDY = await this.measureRefDY();
+    this.refScroller.scrollToPosition(0, refDY);
+  };
+
+  handleEBlur = async () => {
+    await delay();
+    this.refScroller.scrollToEnd();
+  };
+
+  handleBLabelRef = (ref) => {
+    this.refBLabel = ref;
+  };
+
+  handleBInputRef = (ref) => {
+    this.refBInput = ref;
+  };
+
+  handleCLabelRef = (ref) => {
+    this.refCLabel = ref;
+  };
+
+  handleCInputRef = (ref) => {
+    this.refCInput = ref;
+  };
+
+  handleDRef = (ref) => {
+    this.refD = ref;
+  };
+
+  handleELabelRef = (ref) => {
+    this.refELabel = ref;
+  };
+
+  handleEInputRef = (ref) => {
+    this.refEInput = ref;
+  };
+
+  handleScrollerRef = (ref) => {
+    this.refScroller = ref;
+  }
+
+  handleDidFocus = async ({ state: { params: { value } = {} } }) => {
     if (value === undefined) return;
     this.setState({
       valueD: value,
     });
+    const refEY = await this.measureRefEY();
+    this.refScroller.scrollToPosition(0, refEY);
+    await delay();
+    this.refEInput.focus();
   }
 
   render() {
@@ -42,6 +135,7 @@ class Form extends PureComponent {
       valueB,
       valueC,
       valueD,
+      valueE,
     } = this.state;
     return (
       <View style={{ flex: 1 }}>
@@ -49,14 +143,28 @@ class Form extends PureComponent {
           onDidFocus={this.handleDidFocus}
         />
         <FormView
+          onABlur={this.handleABlur}
           onAChangeText={this.handleAChangeText}
+          onBBlur={this.handleBBlur}
           onBChangeText={this.handleBChangeText}
+          onBInputRef={this.handleBInputRef}
+          onBLabelRef={this.handleBLabelRef}
+          onCBlur={this.handleCBlur}
+          onCInputRef={this.handleCInputRef}
+          onCLabelRef={this.handleCLabelRef}
           onCChangeText={this.handleCChangeText}
           onDPress={this.handleDPress}
+          onDRef={this.handleDRef}
+          onEBlur={this.handleEBlur}
+          onEInputRef={this.handleEInputRef}
+          onELabelRef={this.handleELabelRef}
+          onEChangeText={this.handleEChangeText}
+          onScrollerRef={this.handleScrollerRef}
           valueA={valueA}
           valueB={valueB}
           valueC={valueC}
           valueD={valueD}
+          valueE={valueE}
         />
       </View>
     );
